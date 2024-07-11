8c72cad141acc6937ac401a8f6fe54f3c49f6936
davy.tran <davytran1993@gmail.com>
2024-07-11 21:04:23 +0200

    Init prisma service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {
+    //
+  }
+
+  private readonly saltRounds = 10;
+
+  public async register(username: string, password: string) {
+    const hashedPassword = bcrypt.hash(password, this.saltRounds);
+  }
+}
