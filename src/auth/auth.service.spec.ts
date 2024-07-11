94b46f93162d1598b8cb13631cde05eaa94c9aa3
davy.tran <davytran1993@gmail.com>
2024-07-11 20:48:09 +0200

    Add unit tests for the credentials

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { user } from 'src/fixtures/users';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -14,5 +15,19 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Credentials - Basic', async () => {
+    it('should register a new user', async () => {
+      const { username, password } = user;
+      const user = await service.register(username, password);
+      expect(user).toBeDefined();
+    });
+
+    it('should login a user and return the access token', async () => {
+      const { access_token } = await service.login(user);
+      expect(access_token).toBeInstanceOf(String);
+      expect(access_token.length).toBeGreaterThan(0);
+    });
   });
 });
---
+++ src/fixtures/users.ts
@@ -0,0 +1,9 @@
+import { User } from '@prisma/client';
+
+const user: User = {
+  id: 0,
+  username: 'john.doe',
+  password: '@123Abcdef',
+};
+
+export { user };
