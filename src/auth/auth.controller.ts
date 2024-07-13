a21bb093f93e9d29de9934f98a6a70119fa09030
davy.tran <davytran1993@gmail.com>
2024-07-13 21:00:54 +0200

    Fix validation pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CredentialDto, RegisterDto } from './dto/credential.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body) {
-    return this.authService.register(body);
+  async register(@Body() credential: RegisterDto) {
+    return this.authService.register(credential);
   }
 
   @Post('login')
-  async login(@Body() body) {
-    return this.authService.login(body);
+  async login(@Body() credential: CredentialDto) {
+    return this.authService.login(credential);
   }
 }
