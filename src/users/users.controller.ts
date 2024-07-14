4519f939d3bedf666f922cd4efed2a93d275b6dc
davy.tran <davytran1993@gmail.com>
2024-07-14 13:34:03 +0200

    Remove password from response for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,6 +13,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { Roles } from 'src/roles/roles.decorator';
+import { ResponseUser } from './dto/response-user.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('users')
@@ -21,19 +22,19 @@ export class UsersController {
 
   @Roles(['ADMIN'])
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto): Promise<ResponseUser> {
     return this.usersService.create(createUserDto);
   }
 
   @Roles(['ADMIN'])
   @Get()
-  async findAll() {
+  async findAll(): Promise<ResponseUser[]> {
     return this.usersService.findAll();
   }
 
   @Roles(['ADMIN', 'USER'])
   @Get(':user_id')
-  async findOne(@Param('user_id') id: string) {
+  async findOne(@Param('user_id') id: string): Promise<ResponseUser> {
     return this.usersService.findOne(+id);
   }
 
@@ -42,13 +43,13 @@ export class UsersController {
   async update(
     @Param('user_id') id: string,
     @Body() updateUserDto: UpdateUserDto,
-  ) {
+  ): Promise<ResponseUser> {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Roles(['ADMIN', 'USER'])
   @Delete(':user_id')
-  async remove(@Param('user_id') id: string) {
+  async remove(@Param('user_id') id: string): Promise<ResponseUser> {
     return this.usersService.remove(+id);
   }
 }
