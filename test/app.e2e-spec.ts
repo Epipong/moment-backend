e0df279e2865d62ce3eafe24d1e1203c0d420afa
davy.tran <davytran1993@gmail.com>
2024-07-18 09:04:34 +0200

    Add describe to e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -42,106 +42,110 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  describe('POST: /auth/register', () => {
-    beforeEach(() => {
-      jest.spyOn(authService, 'register');
-      jest.spyOn(usersService, 'create');
-    });
-
-    it('should register a new user', async () => {
-      const createdUser: UserEntity = plainToInstance(UserEntity, user);
-      await request(app.getHttpServer())
-        .post('/auth/register')
-        .expect(201)
-        .expect(JSON.stringify(createdUser));
-    });
-  });
-
-  describe('POST: /auth/login', () => {
-    beforeEach(() => {
-      jest.spyOn(authService, 'login');
-      jest.spyOn(usersService, 'findAll');
-    });
-
-    it('should log in the user', async () => {
-      const accessToken = {
-        access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
-      };
-      await request(app.getHttpServer())
-        .post('/auth/login')
-        .expect(201)
-        .expect(JSON.stringify(accessToken));
-    });
-  });
-
-  describe('POST: /users', () => {
-    beforeEach(() => {
-      jest.spyOn(usersService, 'create');
-    });
-
-    it('should create a new user', async () => {
-      const createdUser: UserEntity = plainToInstance(UserEntity, user);
-      await request(app.getHttpServer())
-        .post('/users')
-        .expect(201)
-        .expect(JSON.stringify(createdUser));
-    });
-  });
-
-  describe('PATCH: /users/:id', () => {
-    beforeEach(() => {
-      jest.spyOn(usersService, 'create');
-    });
-
-    it('should update user by id', async () => {
-      const updatedUser: UserEntity = plainToInstance(UserEntity, {
-        ...user,
-        username: 'davy.tran',
+  describe('Authentification', () => {
+    describe('POST: /auth/register', () => {
+      beforeEach(() => {
+        jest.spyOn(authService, 'register');
+        jest.spyOn(usersService, 'create');
       });
-      await request(app.getHttpServer())
-        .patch(`/users/${user.id}`)
-        .expect(200)
-        .expect(JSON.stringify(updatedUser));
+
+      it('should register a new user', async () => {
+        const createdUser: UserEntity = plainToInstance(UserEntity, user);
+        await request(app.getHttpServer())
+          .post('/auth/register')
+          .expect(201)
+          .expect(JSON.stringify(createdUser));
+      });
+    });
+
+    describe('POST: /auth/login', () => {
+      beforeEach(() => {
+        jest.spyOn(authService, 'login');
+        jest.spyOn(usersService, 'findAll');
+      });
+
+      it('should log in the user', async () => {
+        const accessToken = {
+          access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+        };
+        await request(app.getHttpServer())
+          .post('/auth/login')
+          .expect(201)
+          .expect(JSON.stringify(accessToken));
+      });
     });
   });
 
-  describe('DELETE: /users/:id', () => {
-    beforeEach(() => {
-      jest.spyOn(usersService, 'remove');
+  describe('Users', () => {
+    describe('POST: /users', () => {
+      beforeEach(() => {
+        jest.spyOn(usersService, 'create');
+      });
+
+      it('should create a new user', async () => {
+        const createdUser: UserEntity = plainToInstance(UserEntity, user);
+        await request(app.getHttpServer())
+          .post('/users')
+          .expect(201)
+          .expect(JSON.stringify(createdUser));
+      });
     });
 
-    it('should delete user by id', async () => {
-      await request(app.getHttpServer())
-        .delete(`/users/${user.id}`)
-        .expect(200);
-    });
-  });
+    describe('PATCH: /users/:id', () => {
+      beforeEach(() => {
+        jest.spyOn(usersService, 'create');
+      });
 
-  describe('GET: /users', () => {
-    beforeEach(() => {
-      jest.spyOn(usersService, 'findAll');
-    });
-
-    it('should read all users', async () => {
-      const allUsers: UserEntity[] = plainToInstance(UserEntity, users);
-      await request(app.getHttpServer())
-        .get('/users')
-        .expect(200)
-        .expect(JSON.stringify(allUsers));
-    });
-  });
-
-  describe('GET: /users/:id', () => {
-    beforeEach(() => {
-      jest.spyOn(usersService, 'findOne');
+      it('should update user by id', async () => {
+        const updatedUser: UserEntity = plainToInstance(UserEntity, {
+          ...user,
+          username: 'davy.tran',
+        });
+        await request(app.getHttpServer())
+          .patch(`/users/${user.id}`)
+          .expect(200)
+          .expect(JSON.stringify(updatedUser));
+      });
     });
 
-    it('should read user by id', async () => {
-      const foundUser: UserEntity = plainToInstance(UserEntity, user);
-      await request(app.getHttpServer())
-        .get(`/users/${user.id}`)
-        .expect(200)
-        .expect(JSON.stringify(foundUser));
+    describe('DELETE: /users/:id', () => {
+      beforeEach(() => {
+        jest.spyOn(usersService, 'remove');
+      });
+
+      it('should delete user by id', async () => {
+        await request(app.getHttpServer())
+          .delete(`/users/${user.id}`)
+          .expect(200);
+      });
+    });
+
+    describe('GET: /users', () => {
+      beforeEach(() => {
+        jest.spyOn(usersService, 'findAll');
+      });
+
+      it('should read all users', async () => {
+        const allUsers: UserEntity[] = plainToInstance(UserEntity, users);
+        await request(app.getHttpServer())
+          .get('/users')
+          .expect(200)
+          .expect(JSON.stringify(allUsers));
+      });
+    });
+
+    describe('GET: /users/:id', () => {
+      beforeEach(() => {
+        jest.spyOn(usersService, 'findOne');
+      });
+
+      it('should read user by id', async () => {
+        const foundUser: UserEntity = plainToInstance(UserEntity, user);
+        await request(app.getHttpServer())
+          .get(`/users/${user.id}`)
+          .expect(200)
+          .expect(JSON.stringify(foundUser));
+      });
     });
   });
 
